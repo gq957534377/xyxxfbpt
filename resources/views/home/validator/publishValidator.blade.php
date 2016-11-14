4e48754efc46b9cc494a639d235b98fcfed78aed
jacklin <361525737@qq.com>
2016-11-14 02:07:54 +0100

    哈哈

--- resources/views/home/validator/publishValidator.blade.php
+++ resources/views/home/validator/publishValidator.blade.php
@@ -8,54 +8,54 @@
      * Form Validator
      */
     // 文档地址 http://www.runoob.com/jquery/jquery-plugin-validate.html
-    !(function ($) {
-        "use strict";//使用严格标准
-        // 获取表单元素
-        var FormValidator = function(){
-            this.$projectForm = $("#projectForm");
-        };
-        // 初始化
-        FormValidator.prototype.init = function() {
-            this.$projectForm.validate({
-                // 验证规则
-                rules: {
-                    title: {
-                        required: true
-                    },
-                    content: {
-                        required: true
-                    },
-                    image: {
-                        required: true
-                    },
-                    file: {
-                        required: true
-                    }
-                },
-                // 提示信息
-                messages: {
-                    title: {
-                        required: '必须要填写标题哦'
-                    },
-                    content: {
-                        required: '必须要填写项目简介哦'
-                    },
-                    image: {
-                        required: '必须要上传一张图片哦'
-                    },
-                    file: {
-                        required: '必须要上传一份项目文件哦'
-                    }
-                }
-            });
-        };
-        $.FormValidator = new FormValidator;
-        $.FormValidator.Constructor = FormValidator;
-    })(window.jQuery),
-            function($){
-                "use strict";
-                $.FormValidator.init();
-            }(window.jQuery);
+//    !(function ($) {
+//        "use strict";//使用严格标准
+//        // 获取表单元素
+//        var FormValidator = function(){
+//            this.$projectForm = $("#projectForm");
+//        };
+//        // 初始化
+//        FormValidator.prototype.init = function() {
+//            this.$projectForm.validate({
+//                // 验证规则
+//                rules: {
+//                    title: {
+//                        required: true
+//                    },
+//                    content: {
+//                        required: true
+//                    },
+//                    image: {
+//                        required: true
+//                    },
+//                    file: {
+//                        required: true
+//                    }
+//                },
+//                // 提示信息
+//                messages: {
+//                    title: {
+//                        required: '必须要填写标题哦'
+//                    },
+//                    content: {
+//                        required: '必须要填写项目简介哦'
+//                    },
+//                    image: {
+//                        required: '必须要上传一张图片哦'
+//                    },
+//                    file: {
+//                        required: '必须要上传一份项目文件哦'
+//                    }
+//                }
+//            });
+//        };
+//        $.FormValidator = new FormValidator;
+//        $.FormValidator.Constructor = FormValidator;
+//    })(window.jQuery),
+//            function($){
+//                "use strict";
+//                $.FormValidator.init();
+//            }(window.jQuery);
 
 
     !function($) {
