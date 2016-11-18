e1fb8c3c6e638efa386c6adcf2bf7fd2896be829
guoqing <957534377@qq.com>
2016-11-18 07:31:50 +0100

    前台初始化

---
+++ app/Http/Controllers/Home/ActionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Validator;
+use App\Http\Controllers\Controller;
+use App\Services\ActionService as ActionServer;
+
+class ActionController extends Controller
+{
+    protected static $request;
+    protected  static $actionServer;
+    public function __construct(Request $request,ActionServer $actionServer)
+    {
+        self::$actionServer = $actionServer;
+        self::$request = $request;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *向活动表插入数据
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @author 张洵之
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
