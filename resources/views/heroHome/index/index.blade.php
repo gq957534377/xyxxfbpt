8ef7b9731189ab8af07c140871ae6e75d50d9b5b
刘峻廷 <342766475@qq.com>
2016-12-02 10:39:55 +0100

    修改首页头像

--- resources/views/heroHome/index/index.blade.php
+++ resources/views/heroHome/index/index.blade.php
@@ -19,12 +19,12 @@
     <div class="container">
         <div class="row change-row-margin">
             <div class="top-left">
-                <a href="#"><img class="img-responsive" src="{{ asset('heroHome/img/logo.jpg') }}"></a>
+                <a href="{{ url('/') }}"><img class="img-responsive" src="{{ asset('heroHome/img/logo.jpg') }}"></a>
             </div>
             @if(!empty(session('user')))
                 <div class="top-right">
-                    <a href="#"><img class="img-circle" src="{{ session('user')->headpic }}"></a>
-                    <a href="#">未登录</a>
+                    <a href="{{ url('/user') }}"><img class="img-circle" src="{{ session('user')->headpic }}" alt="头像" title="{{ session('user')->nickname }}"></a>
+                    <a href="{{ url('/user') }}">{{ session('user')->nickname }}</a>
                     <a href="#">英雄社区</a>
                 </div>
             @else
