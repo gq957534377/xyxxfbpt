f129269c9ae2acc68ef39955481da2173298220b
luowei <18166471875@163.com>
2016-12-15 18:23:42 +0800

    rewrite user model 2016-12-15

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -12,36 +12,22 @@
 @section('content')
 
     <div class="btn-toolbar" role="toolbar">
-
-        {{--<div class="btn-group">--}}
-            {{--<button type="button" data-name="user_normal" onclick="getInfo(1)" title="普通用户" role="1" status="1" class="user_list btn btn-default">普通用户</button>--}}
-        {{--</div>--}}
-        {{--<div class="btn-group">--}}
-            {{--<button type="button" data-name="user_entrepreneurs" title="创业者用户" onclick="getInfo(2)" role="2" status="1" class="user_list btn btn-default">创业者用户</button>--}}
-        {{--</div>--}}
-        {{--<div class="btn-group">--}}
-            {{--<button type="button" data-name="user_investor" title="投资者用户" role="3" status="1" class="user_list btn btn-default">投资者用户</button>--}}
-        {{--</div>--}}
-        {{--<div class="btn-group">--}}
-            {{--<button type="button" data-name="user_investor" memeber="3" role="memeber" status="1" class=" btn btn-default user_memeber_list" title="英雄会成员">英雄会成员</button>--}}
-        {{--</div>--}}
-
         <div class="btn-group">
             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">用户类型
                 <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" role="menu">
                 <li>
-                    <a  class="user_role_list"   title="普通用户">普通用户</a>
+                    <a  class="user_role_list"  key="0"  title="普通用户">普通用户</a>
                 </li>
                 <li>
-                    <a  class="user_role_list"   title="创业者用户">创业者用户</a>
+                    <a  class="user_role_list"  key="1"  title="创业者用户">创业者用户</a>
                 </li>
                 <li>
-                    <a  class="user_role_list"   title="投资者用户">投资者用户</a>
+                    <a  class="user_role_list"  key="2" title="投资者用户">投资者用户</a>
                 </li>
                 <li>
-                    <a  class="user_role_list"   title="英雄会成员">英雄会成员</a>
+                    <a  class="user_role_list"  key="3"  title="英雄会成员">英雄会成员</a>
                 </li>
             </ul>
         </div>
@@ -51,13 +37,13 @@
             </button>
             <ul class="dropdown-menu" role="menu">
                 <li>
-                    <a  class="user_role_list" title="待审核创业者用户" >创业者</a>
+                    <a  class="user_role_list" key="4" title="待审核创业者用户" >创业者</a>
                 </li>
                 <li>
-                    <a  class="user_role_list" title="待审核投资者用户" >投资者</a>
+                    <a  class="user_role_list" key="5" title="待审核投资者用户" >投资者</a>
                 </li>
                 <li>
-                    <a   class="user_role_list"  title="待审核英雄会成员">英雄会成员</a>
+                    <a   class="user_role_list" key="6"  title="待审核英雄会成员">英雄会成员</a>
                 </li>
             </ul>
         </div>
@@ -67,13 +53,13 @@
             </button>
             <ul class="dropdown-menu" role="menu">
                 <li>
-                    <a class="user_role_list" title="审核失败创业者用户">创业者</a>
+                    <a class="user_role_list" key="7" title="审核失败创业者用户">创业者</a>
                 </li>
                 <li>
-                    <a  class="user_role_list" title="审核失败投资者用户">投资者</a>
+                    <a  class="user_role_list" key="8" title="审核失败投资者用户">投资者</a>
                 </li>
                 <li>
-                    <a class="user_role_list"  title="审核失败英雄会成员">英雄会成员</a>
+                    <a class="user_role_list" key="9" title="审核失败英雄会成员">英雄会成员</a>
                 </li>
             </ul>
         </div>
@@ -83,44 +69,25 @@
             </button>
             <ul class="dropdown-menu" role="menu">
                 <li>
-                    <a class="user_role_list" title="已禁用普通用户" >普通用户</a>
+                    <a class="user_role_list" key="10" title="已禁用普通用户" >普通用户</a>
                 </li>
                 <li>
-                    <a  class="user_role_list" title="已禁用创业者用户" >创业者</a>
+                    <a  class="user_role_list" key="11" title="已禁用创业者用户" >创业者</a>
                 </li>
                 <li>
-                    <a  class="user_role_list" title="已禁用投资者用户" >投资者</a>
+                    <a  class="user_role_list" key="12" title="已禁用投资者用户" >投资者</a>
                 </li>
                 <li>
-                    <a  class="user_role_list"  title="已禁用英雄会成员">英雄会成员</a>
+                    <a  class="user_role_list" key="13" title="已禁用英雄会成员">英雄会成员</a>
                 </li>
             </ul>
         </div>
-        {{--<div class="btn-group">--}}
-            {{--<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">已停用--}}
-                {{--<span class="caret"></span>--}}
-            {{--</button>--}}
-            {{--<ul class="dropdown-menu" role="menu">--}}
-                {{--<li>--}}
-                    {{--<a  class="user_role_list" title="已停用普通用户" >普通用户</a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a  class="user_role_list" title="已停用创业者用户" >创业者</a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a  class="user_role_list" title="已停用投资者用户" >投资者</a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="user_role_list"  title="已停用英雄会成员">英雄会成员</a>--}}
-                {{--</li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
     </div>
 
     {{--<img src="{{asset('admin/images/load.gif')}}" class="loading">--}}
 
     <div class="page-title">
-        <h5 id="user_title" class="title text-center">普通用户</h5>
+        <h5 id="user_title" class="title text-center"></h5>
     </div>
 
     {{--表格盒子开始--}}
@@ -193,24 +160,44 @@
         $('.user_role_list').click(function(){
             //用户列表title
             var title = $(this).attr('title');
+            var key = $(this).attr('key');
+
             $('#user_title').html('<h3>' + title + '</h3>');
-
-            //$('#data').html(htmlStr(data));
-
 
             //获取参数
             var url = '/test/show';  //请求url
 
-
             //初始化请求参数
             var queryString = {
-                key : title
+                key : key
             };
 
             //执行ajax请求
             execAjax( url, queryString, 'get');
-
         });
+
+        /**
+         * 点击分页列表获取点击页用户信息
+         *
+         */
+        function pages() {
+            $('.pagination li').click(function () {
+                var class_name = $(this).prop('class');
+                if(class_name == 'disabled' || class_name == 'active') {
+                    return false;
+                }
+                var url = $(this).children().prop('href');
+
+                //初始化请求参数
+                var queryString = {};
+
+                //执行ajax请求
+                execAjax( url, queryString, 'get');
+                return false;
+
+            });
+        }
+
 
         
         /** ajax请求，通过参数返回不同类型用户的列表
@@ -219,7 +206,7 @@
          * @param   var querystring   json      请求参数，需要提前拼装为json格式
          * @param   var type          string    请求类型（get or post ...）
          *
-         * @return  data              json      接口响应的json格式数据
+         * @return  msg              json      接口响应的json格式数据 msg.ResultData[1] 用户列表，msg.ResultData[0] 分页信息。
          * */
         function execAjax( url, queryString, type) {
             //如果为非GET请求  携带 csrf _token 做请求验证
@@ -234,14 +221,15 @@
             $.ajax({
                 url : url,
                 type : type,
-
                 data : queryString,
                 success : function(msg){
                     //没有该类型用户数据返回提示
-                    //if (data.status == 400) return $('#data').html('暂无数据');
-                    alert(msg.role);
+                    if (msg.StatusCode == 400) return $('#data').html('暂无数据');
+                    //alert(msg.ResultData);
                     //有数据，遍历数据进行DOM操作
-                    $('#data').html(msg);
+                    $('#data').html(htmlStr(msg.ResultData[1]));
+                    $('#page').html(msg.ResultData[0]);
+                    pages();
 
 
                 }
@@ -265,8 +253,8 @@
                     '<thead>' +
                     '<tr>' +
                     '<th>item</th>' +
-                    '<th>昵称</th>' +
                     '<th>姓名</th>' +
+                    '<th>类型</th>' +
                     '<th>性别</th>' +
                     '<th>生日</th>' +
                     '<th>手机</th>' +
@@ -279,18 +267,36 @@
 
             //
 
-            $.each(data, function (i, e) {
+            $.each(data, function (i, v) {
 
-                str += '<tr class="gradeX">'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td>' + + '</td>'+
-                        '<td><a href="javascript:;" data-name="' +  + '" class="user_modify"><button class="btn btn-info btn-xs">修改</button></a></td></tr>';
+                str += '<tr class="gradeX">';
+                str +=  '<td>' + i + '</td>';
+                str +=  '<td>' + v.realname + '</td>';
+
+                if(v.role == 1){
+                    str +=  '<td>普通用户</td>';
+                }
+                if(v.role == 2){
+                    str +=  '<td>创业者</td>';
+                }
+                if(v.role == 3){
+                    str += '<td>投资者</td>';
+                }
+                if(v.role == 4){
+                    str += '<td>英雄会员</td>';
+                }
+
+                if(v.sex==0 ){
+                    str += '<td>男</td>';
+                }else{
+                    str +=  '<td>女</td>';
+                }
+
+                str += '<td>' + v.birthday + '</td>';
+                str +=  '<td>' + v.tel + '</td>';
+                str +=  '<td>' + v.email + '</td>';
+                str +=  '<td>' + '&nbsp;' + '</td>';
+                str +=  '<td><a href="javascript:;" data-name="' + v.guid + '" class="user_modify"><button class="btn btn-info btn-xs">修改</button></a></td></tr>';
             });
             str += '</tbody>' +
                     '</table>' +
@@ -299,7 +305,7 @@
                     '<div class="col-sm-8"></div>' +
                     '<div class="col-sm-4" id="page"></div>' +
                     '</div>';
-
+            return str;
         }
         /**
          *
