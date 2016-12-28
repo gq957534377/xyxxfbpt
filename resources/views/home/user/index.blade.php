d9b2a42a686c1c124436ce42f62aa614802f1c53
guoqing <957534377@qq.com>
2016-12-28 14:15:01 +0100

    12.28合并所有

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -178,6 +178,7 @@
         <!--投资者认证通过 开始-->
 
         <div class="col-xs-12 col-sm-12 col-md-12 pad-clr b-all-3 bgc-0 my-home-info my-home-min{{ (session('user')->role == '23' || session('user')->role == '3') ? 'hidden' : '' }}">
+
             @if(!empty(session('roleInfo')[3] ))
             <h4 class="col-xs-12 pad-clr my-home-title">投资者认证
                 @if(session('roleInfo')[3]->status == 5 )
