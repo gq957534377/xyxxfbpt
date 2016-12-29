283f7026b0c6a3cb3d966ec4259bdf7a29b559f2
zxz <1226580878@qq.com>
2016-12-29 11:58:47 +0100

    sdjghf

--- app/Redis/ProjectCache.php
+++ app/Redis/ProjectCache.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/12/29
- * Time: 17:02
+ * Project redis 缓存仓库
+ * @author 张洵之
  */
 
 namespace App\Redis;
 
-
+use Illuminate\Support\Facades\Redis;
 class ProjectCache
 {
-
+    private static $lkey = LIST_PROJECT_INFO;      //项目list表key
+    private static $hkey = HASH_PROJECT_INFO_;     //项目hash表key
 }
