e4c7abcd46201cf757426226142510817991f367
guoqing <957534377@qq.com>
2017-01-04 19:01:24 +0800

    合并junting

--- resources/views/home/user/syb.blade.php
+++ resources/views/home/user/syb.blade.php
@@ -161,27 +161,27 @@
         });
 
         // 获取用户真实姓名
-        ajax.ajax({
-            url      :   '/user/realname' + '/'+$('#topAvatar').data('id'),
-            type     :   'GET',
-            beforeSend: ajaxBeforeSend($('.loading')),
-            success  :   function(msg){
-                ajaxAfterSend($('.loading'));
-                switch (msg.StatusCode){
-                    case '400':
-                        $('input[name="syb_realname"]').attr('disabled', false);
-                        break;
-                    case '200':
-                        if (msg.ResultData.realname == '') {
-                            $('input[name="syb_realname"]').attr('disabled', false);
-                        }else {
-                            $('input[name="syb_realname"]').val(msg.ResultData.realname).attr('disabled', true);
-                        }
-
-                        break;
-                }
-            },
-        });
+//        ajax.ajax({
+//            url      :   '/user/realname' + '/'+$('#topAvatar').data('id'),
+//            type     :   'GET',
+//            beforeSend: ajaxBeforeSend($('.loading')),
+//            success  :   function(msg){
+//                ajaxAfterSend($('.loading'));
+//                switch (msg.StatusCode){
+//                    case '400':
+//                        $('input[name="syb_realname"]').attr('disabled', false);
+//                        break;
+//                    case '200':
+//                        if (msg.ResultData.realname == '') {
+//                            $('input[name="syb_realname"]').attr('disabled', false);
+//                        }else {
+//                            $('input[name="syb_realname"]').val(msg.ResultData.realname).attr('disabled', true);
+//                        }
+//
+//                        break;
+//                }
+//            },
+//        });
 
     </script>
 @endsection
