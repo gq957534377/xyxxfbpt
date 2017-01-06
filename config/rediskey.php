549caddc0978313dd188506ee29860d5ec318196
zxz <1226580878@qq.com>
2017-01-06 02:27:23 +0100

    redisStart

--- config/rediskey.php
+++ config/rediskey.php
@@ -4,6 +4,8 @@
 //articcle 文章 redis key
 define('LIST_ARTICLE_INFO','LIST:ARTICLE:INFO');
 define('HASH_ARTICLE_INFO_','HASH:ARTICLE:INFO:');
+//project 项目 redis key
+define('LIST_PROJECT_INFO_','LIST:PROJECT:INFO');
 define('HASH_PROJECT_INFO_','HASH:PROJECT:INFO:');
 //action 活动 redis key
 define('LIST_ACTION_INFO','LIST:ACTION:INFO');
