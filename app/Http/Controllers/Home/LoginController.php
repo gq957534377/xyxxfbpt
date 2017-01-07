05e8896f25df938618260543dc827196be77afa2
guoqing <957534377@qq.com>
2017-01-07 18:51:16 +0800

    jdhsk

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Services\UserService as UserServer;
 use App\Tools\Common;
 use App\Tools\Safety;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
