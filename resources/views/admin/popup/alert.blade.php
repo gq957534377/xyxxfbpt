5bf97c379185eec57b874e5f8f97a0d8cb6e6320
guoqing <guoqing@qq.com>
2017-04-08 11:22:47 +0200

    删除无用

--- resources/views/admin/popup/alert.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <button class="btn btn-info btn-sm" id="sa-basic">Click me</button>
-@section('script')
-    <script src="http://cdn.rooyun.com/js/sweet-alert.min.js"></script>
-    <script src="http://cdn.rooyun.com/js/sweet-alert.init.js"></script>
-@endsection
-@endsection
